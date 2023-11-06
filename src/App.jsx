ee90c01954ca6595e0005309690df10754d89956
DevvSakib <devvsakib@gmail.com>
2023-11-06 10:44:08 +0600

    link color change

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ const App = () => {
     return (
         <div className='max-w-[1280px] mx-auto mt-10'>
             <QueryBuilder />
-            <p className='text-center my-10'>
-                Repo: <a href="https://github.com/devvsakib/jsonpath">Check</a>
+            <p className='text-center my-10 text-black'>
+                Repo: <a className='text-orange-500 alink hover:text-green-500'href="https://github.com/devvsakib/jsonpath">Check</a>
             </p>
         </div>
     )
