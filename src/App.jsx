000a68cca8c25756c1e1ff2ca67286a0618f55a3
DevvSakib <devvsakib@gmail.com>
2023-11-06 10:27:20 +0600

    file rename + repo link

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
     return (
         <div className='max-w-[1280px] mx-auto mt-10'>
             <QueryBuilder />
+            <p className='text-center my-10'>
+                Repo: <a href="https://github.com/devvsakib/jsonpath">Check</a>
+            </p>
         </div>
     )
 }
