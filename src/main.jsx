5b4a33e604af45b0a2490740dfcb7833687ba239
devvsakib <devvsakib@gmail.com>
2024-07-19 11:40:28 +0600

    pricing table added

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import TestTable from './TestTable.jsx'
+import Pricing from './pages/Pricing.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-  )
+ReactDOM.createRoot(document.getElementById('root')).render(<Pricing />)
---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,9 @@
+import PricingTable from "./PricingTable";
+
+const Pricing = () => {
+  return <div className="p-5">
+    <PricingTable/>
+  </div>;
+};
+
+export default Pricing;
