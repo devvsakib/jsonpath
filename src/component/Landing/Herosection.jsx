5143249f06eaeb7524be3311845a2dc7616fe15f
devvsakib <devvsakib@gmail.com>
2024-08-22 19:45:43 +0600

    banner logo changes

--- src/component/Landing/Herosection.jsx
+++ src/component/Landing/Herosection.jsx
@@ -4,7 +4,7 @@ const Herosection = () => {
     return <section className="grid mx-auto w-[1380px] items-center px-5 md:px-10 text-center h-screen z-10 relative  mb-20">
         <div className="min-h-[80vh] grid place-content-center">
             <div className="w-fit mx-auto">
-                <img src="/images/landing/welcometext.png" alt="logo" className="w-1/3 mx-auto" />
+                <img src="/images/landing/autobot_border.png" alt="logo" className="ttt w-1/3 mx-auto" />
             </div>
             <h2 className="my-5 text-xl md:text-8xl">Making Automation Easy</h2>
             <p className="text-gray-500 text-xl">Hyperautomate with NoCode, LowCode and FullCode</p>
